e038ae46850ccb11109d29678f1c306bfb82ccf8
Frank Cooke <frankcooke79@gmail.com>
2019-07-20 10:45:50 -0400

    removing fetch for schema from mocking provider component, bump version

--- src/ApolloMockingProvider.tsx
+++ src/ApolloMockingProvider.tsx
@@ -3,33 +3,26 @@ import ApolloClient from 'apollo-client';
 import SchemaLink from 'apollo-link-schema';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from 'react-apollo-hooks';
-import { IMockOptions } from 'graphql-tools';
-import { Omit } from 'react-apollo-hooks/lib/utils';
-import { useFetchGraphQLSchema } from 'utils/useFetchGraphQLSchema';
+import {
+  addMockFunctionsToSchema,
+  IMockOptions,
+  ITypeDefinitions,
+  makeExecutableSchema,
+} from 'graphql-tools';
 
 type MockOptions = Omit<IMockOptions, 'schema'>;
 
 interface ApolloMockingProviderProps extends MockOptions {
-  url: string;
+  typeDefs: ITypeDefinitions;
 }
 
 export const ApolloMockingProvider: React.FC<ApolloMockingProviderProps> = ({
-  url,
+  typeDefs,
   children,
   ...mockOptions
 }) => {
-  const { schema, error } = useFetchGraphQLSchema(url, mockOptions);
-
-  console.log('schema', schema);
-  console.log('error', error);
-
-  if (error) {
-    throw new Error('ApolloMockingProvider: error fetching schema');
-  }
-
-  if (!schema) {
-    throw new Error('ApolloMockingProvider: fetched schema is undefined');
-  }
+  const schema = makeExecutableSchema({ typeDefs });
+  addMockFunctionsToSchema({ schema, ...mockOptions });
 
   const client = new ApolloClient({
     link: new SchemaLink({ schema }),
