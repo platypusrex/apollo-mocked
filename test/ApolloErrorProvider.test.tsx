1f4713a24d796c0a6507ab836e6b7dffa2e72626
Frank Cooke <frankcooke79@gmail.com>
2020-09-04 18:51:59 -0400

    fix: issue with apollo esm exporting of testing utilities

--- test/ApolloErrorProvider.test.tsx
+++ test/ApolloErrorProvider.test.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { GraphQLError } from 'graphql';
-import { render, waitFor } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import { Component } from './Component';
-import { ApolloErrorProvider } from '../src';
+import { ApolloErrorProvider } from '../src/ApolloErrorProvider';
 
 describe('ApolloErrorProvider', () => {
   it('should render the default graphql error message', async () => {
-    const { getByText } = render(
+    render(
       <ApolloErrorProvider>
         <Component />
       </ApolloErrorProvider>
@@ -14,32 +14,32 @@ describe('ApolloErrorProvider', () => {
 
     await waitFor(() => {
       expect(
-        getByText('GraphQL error: Unspecified error from ErrorProvider.')
+        screen.getByText('Unspecified error from ErrorProvider.')
       ).toBeTruthy();
     });
   });
 
   it('should render a graphql error message with provided string', async () => {
-    const { getByText } = render(
+    render(
       <ApolloErrorProvider graphQLError="Dog not found.">
         <Component />
       </ApolloErrorProvider>
     );
 
     await waitFor(() => {
-      expect(getByText('GraphQL error: Dog not found.')).toBeTruthy();
+      expect(screen.getByText('Dog not found.')).toBeTruthy();
     });
   });
 
   it('should render a graphql error message with provided graphql error', async () => {
-    const { getByText } = render(
+    render(
       <ApolloErrorProvider graphQLError={[new GraphQLError('Dog not found.')]}>
         <Component />
       </ApolloErrorProvider>
     );
 
     await waitFor(() => {
-      expect(getByText('GraphQL error: Dog not found.')).toBeTruthy();
+      expect(screen.getByText('Dog not found.')).toBeTruthy();
     });
   });
 });
